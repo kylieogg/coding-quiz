d3a8f33005dde93f961745c689e72d0b1544cc38
kylieogg <kylieogg1@gmail.com>
2022-07-09 18:11:04 -0400

    entered all quiz questions into script

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        a: "strings",
+        b: "booleans",
+        c: "alerts",
+        d: "numbers",
+        correct: "c",
+
+        question: "The condition in an if/else statesment is enclosed with __________.",
+        a: "quotes",
+        b: "curly brackets",
+        c: "parenthesis",
+        d: "square brackets",
+        correct: "b",
+
+        question: "Arrays in JavaScript can be used to store _______________.",
+        a: "numbers and strings",
+        b: "other arrays",
+        c: "booleans",
+        d: "all of the above",
+        correct: "d",
+
+        question: "String values must be enclosed within ____________ when being assigned to variables.",
+        a: "commas",
+        b: "curly brackets",
+        c: "quotes",
+        d: "parenthesis",
+        correct: "c",
+
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        a: "JavaScript",
+        b: "terminal/bash",
+        c: "for loops",
+        d: "console.log",
+        correct: "d",
+    }
+]
