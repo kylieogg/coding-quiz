94ce596c763802352e4b6327caf87f140b25848d
kylieogg <kylieogg1@gmail.com>
2022-07-09 20:50:51 -0400

    updated html to include an intro slide to the quiz

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let quizData = [
+/* ALL questions, answer choices, and correct answers included in quiz */
+const quizData = [
     {
         question: "Commonly used data types DO NOT include:",
         a: "strings",
@@ -43,15 +44,69 @@ let quizData = [
 ];
 
 /* Variables for questions and answers */
-let quiz= document.getElementById('quiz')
-let answer= document.querySelectorAll('.answer')
-let question= document.getElementById('question')
+const quiz= document.getElementById('quiz')
+const answerEls= document.querySelectorAll('.answer')
+const questionEl= document.getElementById('question')
 
 /* Variables for possible answers */
-let a_text= document.getElementById('a_text')
-let b_text= document.getElementById('b_text')
-let c_text= document.getElementById('c_text')
-let d_text= document.getElementById('d_text')
+const a_text= document.getElementById('a_text')
+const b_text= document.getElementById('b_text')
+const c_text= document.getElementById('c_text')
+const d_text= document.getElementById('d_text')
 
 /* Variable for submit button */
-let submitBtn= document.getElementById('submit')
+const submitBtn= document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+/* Function to call quiz to load questions */
+function loadQuiz(){
+    deselectAnswers()
+
+    const currentQuizData= quizData[currentQuiz]
+    
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++
+        }
+
+        currentQuiz++
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            quiz.innerHTML = `
+            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+
+            <button onclick="location.reload()">Reload</button>
+            `
+        }
+    }
+})
