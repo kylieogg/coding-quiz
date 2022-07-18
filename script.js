fd72b8ffee654366ef94200b0b167f9b1be98764
kylieogg <kylieogg1@gmail.com>
2022-07-18 14:14:18 -0400

    updated questions array and functions, added timer

--- script.js
+++ script.js
@@ -2,57 +2,60 @@
 const quizData = [
     {
         question: "Commonly used data types DO NOT include:",
-        a: "strings",
-        b: "booleans",
-        c: "alerts",
-        d: "numbers",
+         choices:["strings",
+         "booleans",
+         "alerts",
+         "numbers"],
         correct: "c",
     },
     {
         question: "The condition in an if/else statesment is enclosed with __________.",
-        a: "quotes",
-        b: "curly brackets",
-        c: "parenthesis",
-        d: "square brackets",
+         choices:["quotes",
+         "curly brackets",
+         "parenthesis",
+         "square brackets"],
         correct: "b",
     },
     {
         question: "Arrays in JavaScript can be used to store _______________.",
-        a: "numbers and strings",
-        b: "other arrays",
-        c: "booleans",
-        d: "all of the above",
+         choices:["numbers and strings",
+         "other arrays",
+         "booleans",
+         "all of the above"],
         correct: "d",
     },
     {
         question: "String values must be enclosed within ____________ when being assigned to variables.",
-        a: "commas",
-        b: "curly brackets",
-        c: "quotes",
-        d: "parenthesis",
+         choices:["commas",
+         "curly brackets",
+         "quotes",
+         "parenthesis"],
         correct: "c",
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        a: "JavaScript",
-        b: "terminal/bash",
-        c: "for loops",
-        d: "console.log",
+         choices:["JavaScript",
+         "terminal/bash",
+         "for loops",
+         "console.log"],
         correct: "d",
     }
     
 ];
 
+var currentQuestionIndex= 0;
+var timer= 75;
+var timeId;
+
 /* Variables for questions and answers */
-const quiz= document.getElementById('quiz')
-const answerEls= document.querySelectorAll('.answer')
-const questionEl= document.getElementById('question')
+var quiz= document.getElementById('quiz')
+var questionEl= document.getElementById('questions')
+var startBtn= document.getElementById('startBtn')
+var choicesEl= document.getElementById('choices')
+var timeLeft= document.getElementById('time-left')
 
 /* Variables for possible answers */
-const a_text= document.getElementById('a_text')
-const b_text= document.getElementById('b_text')
-const c_text= document.getElementById('c_text')
-const d_text= document.getElementById('d_text')
+
 
 /* Variable for submit button */
 const submitBtn= document.getElementById('submit')
@@ -60,21 +63,64 @@ const submitBtn= document.getElementById('submit')
 let currentQuiz = 0
 let score = 0
 
-loadQuiz()
+startQuiz();
 
 /* Function to call quiz to load questions */
-function loadQuiz(){
-    deselectAnswers()
+function startQuiz(){
+    // Setting a class attribute of hidden to make it disappear.
+    var startScreen= document.getElementById('start-screen');
+    startScreen.setAttribute('class', 'hidden');
 
-    const currentQuizData= quizData[currentQuiz]
-    
-    questionEl.innerText = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
+    questionEl.removeAttribute('class');
 
+    //starting timer
+    timerId = setInterval(startTimer, 1000);
+
+    timeLeft.textContent= timer;
+
+    getCurrentQuestion();
+} 
+
+function getCurrentQuestion() {
+    // Get current question from array
+    var currentQuestion = quizData[currentQuestionIndex];
+    // Setting div element in HTML equal to our question property from quiz data
+    var questionTitle = document.getElementById('question-title');
+    questionTitle.textContent = currentQuestion.question;
+    // Clears previous choices
+    choicesEl.innerHTML = "";
+
+    // 
+    currentQuestion.choices.forEach(function(choice, i){
+        var choiceButton = document.createElement('button');
+        choiceButton.setAttribute('class', 'choice');
+        choiceButton.setAttribute('value', choice);
+
+        choiceButton.textContent = i + 1 + "." + choice;
+
+        choiceButton.onClick = userChoice;
+
+        choicesEl.appendChild(choiceButton);
+    });
 }
+
+function startTimer(){
+    timer--;
+    timeLeft.textContent = timer;
+
+    if (timer <= 0){
+        console.log("game over");
+    }
+}
+//     const currentQuizData= quizData[currentQuiz]
+    
+//     questionEl.innerText = currentQuizData.question
+//     a_text.innerText = currentQuizData.a
+//     b_text.innerText = currentQuizData.b
+//     c_text.innerText = currentQuizData.c
+//     d_text.innerText = currentQuizData.d
+
+// }
 
 function deselectAnswers() {
     answerEls.forEach(answerEl => answerEl.checked = false)
@@ -110,3 +156,5 @@ submitBtn.addEventListener('click', () => {
         }
     }
 })
+
+startBtn.onClick = startQuiz;
